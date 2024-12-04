4c76b761d015b48f38003009374f5807318b4724
babypopolinny <littlechubbierabbit@gmail.com>
2024-12-04 16:22:32 +0700

    Add files via upload

---
+++ 1form_35ploy.py
@@ -0,0 +1,10 @@
+print("Welcome to code land")
+
+print("\n\nสวัสดีค่ะ กรุณากรอกข้อมูลส่วนตัวของคุณ ^^")
+nickname = input("ชื่อเล่น")
+height = input ("ส่วนสูง")
+weight = input ("น้ำหนัก")
+
+print("\n\nยินดีที่ได้รู้จักค่ะ คุณ", nickname )
+print("ส่วนสูง ", height,"\n น้ำหนัก", weight)
+print ("\n\n ส่วนสูงกับน้ำหนักกำลังน่ารักเลย จริงๆนะ  เทคแคร์คั้บ จุ้บๆ!!")
